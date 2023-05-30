7fcaa4acfbb5dc3612bb9b004e6882a8ca208307
Melike Kızılcık <melikeekizilcikk@gmail.com>
2023-05-30 16:09:54 +0300

    app.tsx commited

--- App.tsx
+++ App.tsx
@@ -1,38 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView, StyleProp, StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
-import LoginScreen from './screens/LoginScreen';
-import MessageScreen from './screens/MessageScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import PetsScreen from './screens/PetsScreen';
+import {  useFonts, Inter_900Black,  Inter_400Regular,Inter_500Medium, Inter_600SemiBold,} from '@expo-google-fonts/inter';
 import SitterScreen from './screens/SittersScreen';
 
 
-
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,  Inter_400Regular, Inter_500Medium, Inter_600SemiBold,
+  });
 
-  const Stack = createNativeStackNavigator();
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Sitters" screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Pets" component={PetsScreen} />
-        <Stack.Screen name="Sitters" component={SitterScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Message" component={MessageScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <HomeScreen />
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
